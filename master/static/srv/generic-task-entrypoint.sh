3c4ed2672e711087aa43b2c6054ec7bf50fc34ed
Ilia Glazkov <ilia@hpe.com>
2024-01-17 13:13:23 -0800

    generic tasks: support startup hooks

--- master/static/srv/generic-task-entrypoint.sh
+++ master/static/srv/generic-task-entrypoint.sh
@@ -4,6 +4,8 @@ source /run/determined/task-setup.sh
 
 set -e
 
+STARTUP_HOOK="startup-hook.sh"
+export PATH="/run/determined/pythonuserbase/bin:$PATH"
 if [ -z "$DET_PYTHON_EXECUTABLE" ]; then
     export DET_PYTHON_EXECUTABLE="python3"
 fi
@@ -13,6 +15,10 @@ fi
 # which contains the "determined/exec/prep_container.py" script that's needed
 # to register the proxy with the Determined master.
 "$DET_PYTHON_EXECUTABLE" -m determined.exec.prep_container --proxy --download_context_directory
+
+set -x
+test -f "${STARTUP_HOOK}" && source "${STARTUP_HOOK}"
+set +x
 
 if [ "$#" -eq 1 ]; then
     exec /bin/sh -c "$@"
